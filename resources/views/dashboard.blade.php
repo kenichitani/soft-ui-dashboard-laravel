ff1eb98468f9540d322757f4012ef34b1f91d340
kenichitani <kenichi.tani@toyoko-inn.com>
2025-03-21 11:52:49 +0900

    暫定Commit2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -77,7 +77,7 @@
               <div class="numbers">
                 <p class="text-sm mb-0 text-capitalize font-weight-bold">３月売上予測</p>
                 <h5 class="font-weight-bolder mb-0">
-                  ￥10,8719,650&nbsp;<br>
+                  ￥108,719,650&nbsp;<br>
                   <span class="text-success text-sm font-weight-bolder">　</span>
                 </h5>
               </div>
